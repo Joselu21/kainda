f69f71ddfa81c93f5db4b52244f8cb867cba91e3
Jose Luis Silvestre <joseluissilvestregarcia@gmail.com>
2026-01-20 21:11:44 +0100

    .

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const GenericControllers = require("./src/generic.controllers");
-const KaindaModel = require("./src/model");
-const KaindaTransaction = require("./src/model/src/transaction/transaction");
-const KaindaUtils = require("./src/utils");
-const { 
-    ExceptionHandler, 
+const GenericControllers = require("./src/generic.controllers.js");
+const KaindaModel = require("./src/model/index.js");
+const KaindaTransaction = require("./src/model/src/transaction/transaction.js");
+const KaindaUtils = require("./src/utils.js");
+const {
+    ExceptionHandler,
     KaindaException,
     GenericKaindaExceptionHandler,
     GenericKaindaExceptions,
-} = require("./src/exceptions");
-const { KaindaMiddlewares, KaindaMiddlewareUtils} = require("./src/middlewares.utils");
-const AuthFunctions = require("./src/auth.functions");
+} = require("./src/exceptions/index.js");
+
+const { KaindaMiddlewares, KaindaMiddlewareUtils } = require(
+    "./src/middlewares.utils.js",
+);
+const AuthFunctions = require("./src/auth.functions.js");
 const chalk = require("chalk");
 
 module.exports = {
@@ -23,9 +26,9 @@ module.exports = {
     ExceptionHandler,
     GenericKaindaExceptionHandler,
     GenericKaindaExceptions,
-    
+
     ...KaindaMiddlewareUtils,
     ...KaindaMiddlewares,
     ...AuthFunctions,
-    chalk
+    chalk,
 };
